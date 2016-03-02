dd6f1dd64432255f5b753927bff1362a618e26c5
John Tran <johnjtran@icloud.com>
2016-03-01 20:12:05 -0800

    Fix indentation

--- vagrant/share/MPI/potatoes.c
+++ vagrant/share/MPI/potatoes.c
@@ -10,7 +10,7 @@ int name_len;
 
 void printArray(int *N, int size)
 {
-	int i = 0;
+  int i = 0;
   printf("[%s] %d/%d N = [ ", processor_name, my_rank, world_size);
   for (i = 0; i < size; i++) {
     printf("%d ", N[i]);
@@ -30,18 +30,18 @@ int main(int argc, char** argv)
 
   int right = (my_rank + 1) % world_size;
   int size = (argc > 1 ? atoi(argv[1]) : 2);
-	int *N = NULL;
+  int *N = NULL;
   int i = 0;
 
   if (my_rank == 0) {
     int left = world_size - 1;
-		// allocate an array N with random numbers between 0 and 10
+    // allocate an array N with random numbers between 0 and 10
     N = (int*) malloc(sizeof(int) * size);
     for (i = 0; i < size; i++) {
       N[i] = rand() % 10;
     }
 
-		printArray(N, size);
+    printArray(N, size);
 
     // send size and array to my right
     MPI_Send(&size, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
@@ -51,24 +51,24 @@ int main(int argc, char** argv)
     MPI_Recv(&size, 1, MPI_INT, left, 0, MPI_COMM_WORLD, &status);
     MPI_Recv(N, size, MPI_INT, left, 0, MPI_COMM_WORLD, &status);
 
-		printArray(N, size);
+    printArray(N, size);
   } else {
-  	int left = my_rank - 1;
+    int left = my_rank - 1;
 
     // receive size from my left
     MPI_Recv(&size, 1, MPI_INT, left, 0, MPI_COMM_WORLD, &status);
 
-		// allocate array N of size
+    // allocate array N of size
     N = (int*) malloc(sizeof(int) * size);
     MPI_Recv(N, size, MPI_INT, left, 0, MPI_COMM_WORLD, &status);
-		printArray(N, size);
+    printArray(N, size);
 
-		// increment the array of potatoes by +1 for each element
+    // increment the array of potatoes by +1 for each element
     for (i = 0; i < size; i++) {
       N[i]++;
     }
 
-		// send the +1 values to the right
+    // send the +1 values to the right
     MPI_Send(&size, 1, MPI_INT, right, 0, MPI_COMM_WORLD);
     MPI_Send(N, size, MPI_INT, right, 0, MPI_COMM_WORLD);
   }
